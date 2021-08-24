5f6531879ed97852a0640c8c68297198c0f66b2e
Corwin <corwin_hope@yahoo.com>
2021-08-24 11:56:41 -0400

    make it better

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 const cookieParser = require('cookie-parser');
 var cors = require("cors");
 
+
 var whitelist = ['http://127.0.0.1:5500', 'http://localhost:5000', 'https://easy-grapher.herokuapp.com']
 var corsOptions = {
    origin: function (origin, callback) {
