27bc27107fc05858016f8a18408b5d5e411f51ae
Corwin <corwin_hope@yahoo.com>
2022-06-04 13:46:52 -0400

    make it better

--- server.js
+++ server.js
@@ -5,42 +5,43 @@ This server constructs a mongoDb database and permits whitelisted clients to acc
 //Remember to set heroku env. variables for production.
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
-  //process.env.FILESYSTEM_DATABASE_URL = process.env.FILESYSTEM_LOCAL_DATABASE_URL;  
+  //process.env.FILESYSTEM_DATABASE_URL = process.env.FILESYSTEM_LOCAL_DATABASE_URL;
 }
 
 const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 var cors = require("cors");
 
-var whitelist = ['http://127.0.0.1:5500', 'http://localhost:5000', 'https://easy-grapher.herokuapp.com']
+var whitelist = [
+  "http://127.0.0.1:5500",
+  "http://localhost:5000",
+  "https://easy-grapher.herokuapp.com",
+];
 var corsOptions = {
-   origin: function (origin, callback) {
+  origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
-  } 
-}
+  },
+};
 app.use(cors(corsOptions));
 
 app.use(cookieParser());
 
-app.use(express.json({limit: "140mb", extended: true }));
+app.use(express.json({ limit: "140mb", extended: true }));
 
 //const fs = require("./filesystem-mongo");
 const fs = require("file-system-mongo");
 //const fileSystemRoutes = require("./file-system-routes")(fs);
 const fileSystemRoutes = require("file-system-routes")(fs);
-app.use(fileSystemRoutes); 
+app.use(fileSystemRoutes);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const server = app.listen(port, () => {
-  console.log("Listening on port " + port);  
+  console.log("Listening on port " + port);
 });
-
-
- 
